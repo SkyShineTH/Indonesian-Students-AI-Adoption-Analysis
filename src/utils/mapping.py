92bd93de7366b5e0663bdf404656369718a9691e
SkyShineTH <oat606606oat@gmail.com>
2025-08-21 22:31:54 +0700

    feat: clean dataset

---
+++ src/utils/mapping.py
@@ -0,0 +1,86 @@
+# Gender
+gender_map = {
+    1: "Male",
+    2: "Female"
+}
+
+# University
+university_map = {
+    1: "Public",
+    2: "Private"
+}
+
+# Level of Education
+education_map = {
+    1: "Associate Degree (D3)",
+    2: "Undergraduate (S1)"
+}
+
+# Province
+province_map = {
+    1: "Nanggroe Aceh Darussalam (Banda Aceh)",
+    2: "North Sumatera (Medan)",
+    3: "South Sumatera (Palembang)",
+    4: "Sumatera Barat (Padang)",
+    5: "Bengkulu (Bengkulu)",
+    6: "Riau (Pekanbaru)",
+    7: "Riau Islands (Tanjung Pinang)",
+    8: "Jambi (Jambi)",
+    9: "Lampung (Bandar Lampung)",
+    10: "Bangka Belitung (Pangkal Pinang)",
+    11: "West Kalimantan (Pontianak)",
+    12: "East Kalimantan (Samarinda)",
+    13: "South Kalimantan (Banjarbaru)",
+    14: "Central Kalimantan (Palangkaraya)",
+    15: "North Kalimantan (Tanjung Selor)",
+    16: "Banten (Serang)",
+    17: "Jakarta Special Region (Jakarta)",
+    18: "West Java (Bandung)",
+    19: "Central Java (Semarang)",
+    20: "Yogyakarta Special Region (Yogyakarta)",
+    21: "East Java (Surabaya)",
+    22: "Bali (Denpasar)",
+    23: "East Nusa Tenggara (Kupang)",
+    24: "West Nusa Tenggara (Mataram)",
+    25: "Gorontalo (Gorontalo)",
+    26: "West Sulawesi (Mamuju)",
+    27: "Central Sulawesi (Palu)",
+    28: "North Sulawesi (Manado)",
+    29: "Southeast Sulawesi (Kendari)",
+    30: "South Sulawesi (Makassar)",
+    31: "North Maluku (Sofifi)",
+    32: "Maluku (Ambon)",
+    33: "West Papua (Manokwari)",
+    34: "Papua (Jayapura)",
+    35: "Central Papua (Nabire)",
+    36: "Highland Papua (Jayawijaya)",
+    37: "South Papua (Merauke)"
+}
+
+# Fields of Study
+field_map = {
+    1: "Science (Biology, Chemistry, Physics, Medicine, Psychology, Health, Agriculture)",
+    2: "Social Science (Law, Religious Education, Geography, History, Communication Science, Tourism, IR, Sociology, Civic Ed.)",
+    3: "Engineering (Geological, Civil, IT, Electrical, Computer, Mining, Sports Science)",
+    4: "Mathematics (Statistics, Math Education)",
+    5: "Language (English/Japanese Literature & Education)",
+    6: "Economics (Economics, Management, Marketing, Economic Education)",
+    7: "Education (PGSD, PGPAUD, Psychology, Guidance & Counseling)",
+    8: "Arts (Dance, Music, Painting, Visual Design, Drama, Arts Education)"
+}
+
+# Types of AI
+ai_map = {
+    1: "Dall-e",
+    2: "Canva - IA",
+    3: "ChatGPT",
+    4: "DeepL",
+    5: "Bing",
+    6: "Tome.app",
+    7: "Gamma",
+    8: "Sheet Plus",
+    9: "Eduten",
+    10: "Shutterstock AI",
+    11: "Perplexity.AI",
+    12: "Others"
+}
